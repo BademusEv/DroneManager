f9fb2bac29178e25f5842d9c447fe7a8b38f274c
VadimEvteev <evteewadim@gmail.com>
2023-07-24 22:06:06 +0400

    init spring project

---
+++ src/main/java/com/musala/dronemanagerservice/DroneManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musala.dronemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/dronemanagerservice/DroneManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronemanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
