d6f85e3c8f654f06c350046e183676f5652b6ee6
VadimEvteev <evteewadim@gmail.com>
2023-07-26 12:39:49 +0400

    add database connection

--- src/main/java/com/musala/dronemanagerservice/DroneManagerServiceApplication.java
+++ src/main/java/com/musala/dronemanagerservice/DroneManagerServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DroneManagerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroneManagerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DroneManagerServiceApplication.class, args);
+    }
 
 }
