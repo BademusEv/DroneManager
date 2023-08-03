9618c7af6bfb65d72f5cb76dfbcb268a4fc86cf4
VadimEvteev <evteewadim@gmail.com>
2023-08-03 18:50:52 +0400

    add profile for mock of Drone API

--- src/main/java/com/musala/dronemanagerservice/mock/DroneBatteryController.java
+++ src/main/java/com/musala/dronemanagerservice/mock/DroneBatteryController.java
@@ -2,12 +2,14 @@ package com.musala.dronemanagerservice.mock;
 
 import com.musala.dronemanagerservice.model.dto.BatteryDto;
 import java.util.random.RandomGenerator;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Profile("demo")
 @RequestMapping("mock/drone/battery")
 public class DroneBatteryController {
 
